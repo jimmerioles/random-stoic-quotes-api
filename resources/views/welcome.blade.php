28c9e05b4b015addfbe5cd8482c453739dca1bdb
Jim Merioles <jimwisleymerioles@gmail.com>
2017-12-03 23:07:14 +0800

    Edit landing page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Random Quotes API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/jimmerioles/random-quotes-api">GitHub</a>
+                    <a href="https://twitter.com/jimmerioles">Twitter</a>
                 </div>
             </div>
         </div>
