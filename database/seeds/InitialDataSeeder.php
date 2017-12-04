4c1aea8f69e70c56c085bdbdb433541961f65347
Jim Merioles <jimwisleymerioles@gmail.com>
2017-12-05 05:34:05 +0800

    Change image url to filename.

--- database/seeds/InitialDataSeeder.php
+++ database/seeds/InitialDataSeeder.php
@@ -13,7 +13,7 @@ class InitialDataSeeder extends Seeder
     {
         DB::table('authors')->insert([
             'name' => 'Robert Downey Jr.',
-            'image' => url('storage/images', 'robertdowneyjr-m.jpg')
+            'image' => 'robertdowneyjr-m.jpg'
         ]);
         DB::table('quotes')->insert([
             'text' => 'Remember that just because you hit bottom doesn’t mean you have to stay there.',
@@ -22,7 +22,7 @@ class InitialDataSeeder extends Seeder
 
         DB::table('authors')->insert([
             'name' => 'Capt. Jack Sparrow',
-            'image' => url('storage/images', 'captjacksparrow-m.jpg')
+            'image' => 'captjacksparrow-m.jpg'
         ]);
         DB::table('quotes')->insert([
             'text' => 'The problem is not the problem, the problem is your attitude about the problem.',
@@ -31,35 +31,11 @@ class InitialDataSeeder extends Seeder
 
         DB::table('authors')->insert([
             'name' => 'Georges St-Pierre',
-            'image' => url('storage/images', 'georgesstpierre-m.jpg')
+            'image' => 'georgesstpierre-m.jpg'
         ]);
         DB::table('quotes')->insert([
             'text' => 'I trained myself to fight an army, so one guy will not defeat me.',
             'author_id' => 3
         ]);
-
-        // $author = factory(\App\Author::class)
-        //     ->create(['name' => 'Robert Downey Jr.', 'image' => url('storage/images', 'robertdowneyjr-m.jpg')]);
-        // factory(\App\Quote::class)
-        //     ->create([
-        //         'text' => 'Remember that just because you hit bottom doesn’t mean you have to stay there.',
-        //         'author_id' => $author->id
-        //     ]);
-
-        // $author = factory(\App\Author::class)
-        //     ->create(['name' => 'Capt. Jack Sparrow', 'image' => url('storage/images', 'captjacksparrow-m.jpg')]);
-        // factory(\App\Quote::class)
-        //     ->create([
-        //         'text' => 'The problem is not the problem, the problem is your attitude about the problem.',
-        //         'author_id' => $author->id
-        //     ]);
-
-        // $author = factory(\App\Author::class)
-        //     ->create(['name' => 'Georges St-Pierre', 'image' => url('storage/images', 'georgesstpierre-m.jpg')]);
-        // factory(\App\Quote::class)
-        //     ->create([
-        //         'text' => 'I trained myself to fight an army, so one guy will not defeat me.',
-        //         'author_id' => $author->id
-        //     ]);
     }
 }
--- tests/Feature/RespondsWithJsonQuotesTest.php
+++ tests/Feature/RespondsWithJsonQuotesTest.php
@@ -23,7 +23,7 @@ class RespondsWithJsonQuotesTest extends TestCase
 
     private function actual() {
         $author = factory(\App\Author::class)
-            ->create(['name' => 'Robert Downey Jr.', 'image' => url('storage/images', 'robertdowneyjr-m.jpg')]);
+            ->create(['name' => 'Robert Downey Jr.', 'image' => 'robertdowneyjr-m.jpg']);
         factory(\App\Quote::class)
             ->create([
                 'text' => 'Remember that just because you hit bottom doesn’t mean you have to stay there.',
@@ -31,7 +31,7 @@ class RespondsWithJsonQuotesTest extends TestCase
             ]);
 
         $author = factory(\App\Author::class)
-            ->create(['name' => 'Capt. Jack Sparrow', 'image' => url('storage/images', 'captjacksparrow-m.jpg')]);
+            ->create(['name' => 'Capt. Jack Sparrow', 'image' => 'captjacksparrow-m.jpg']);
         factory(\App\Quote::class)
             ->create([
                 'text' => 'The problem is not the problem, the problem is your attitude about the problem.',
@@ -39,7 +39,7 @@ class RespondsWithJsonQuotesTest extends TestCase
             ]);
 
         $author = factory(\App\Author::class)
-            ->create(['name' => 'Georges St-Pierre', 'image' => url('storage/images', 'georgesstpierre-m.jpg')]);
+            ->create(['name' => 'Georges St-Pierre', 'image' => 'georgesstpierre-m.jpg']);
         factory(\App\Quote::class)
             ->create([
                 'text' => 'I trained myself to fight an army, so one guy will not defeat me.',
@@ -88,7 +88,7 @@ class RespondsWithJsonQuotesTest extends TestCase
                     'id' => 1,
                     'attributes' => [
                         'name' => 'Robert Downey Jr.',
-                        'image' => url('storage/images', 'robertdowneyjr-m.jpg')
+                        'image' => 'robertdowneyjr-m.jpg'
                     ]
                 ],
                 [
@@ -96,7 +96,7 @@ class RespondsWithJsonQuotesTest extends TestCase
                     'id' => 2,
                     'attributes' => [
                         'name' => 'Capt. Jack Sparrow',
-                        'image' => url('storage/images', 'captjacksparrow-m.jpg')
+                        'image' => 'captjacksparrow-m.jpg'
                     ]
                 ],
                 [
@@ -104,7 +104,7 @@ class RespondsWithJsonQuotesTest extends TestCase
                     'id' => 3,
                     'attributes' => [
                         'name' => 'Georges St-Pierre',
-                        'image' => url('storage/images', 'georgesstpierre-m.jpg')
+                        'image' => 'georgesstpierre-m.jpg'
                     ]
                 ]
             ],
