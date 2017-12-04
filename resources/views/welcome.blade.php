3b27eaf533317fc78acf312f51e1687e2cba62ab
Jim Merioles <jimwisleymerioles@gmail.com>
2017-12-05 05:59:24 +0800

    Edit readme.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Inspiring Random Quotes API</title>
+        <title>Random Inspiring Quotes API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,7 +69,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Inspiring Random Quotes API
+                    Random Inspiring Quotes API
                 </div>
 
                 <div class="links">
